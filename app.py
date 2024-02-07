5f508a8ca553272684dd0c6ebb8f09ed048a78a7
Prasad <prasad@pschaudhari1712.com>
2024-02-07 10:11:12 +0530

    modified app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 import streamlit as st
+import pdfplumber
+from PyPDF2 import PdfReader
+
+st.title("Chat with PDF...")
+st.write("Upload a PDF and start chatting!") 
+
+def extract_text_pdf(pdf):
+    pdf_reader = PdfReader(pdf)
+    return ''.join(page.extract_text() for page in pdf_reader.pages)
+
+def main():
+    pdf = st.file_uploader('Choose your .pdf file', type="pdf")
+    if pdf : text = extract_text_pdf(pdf)
+    st.write("PDF Content Extracted!")
+    st.write(text)
+
+if __name__ == '__main__': main() 
